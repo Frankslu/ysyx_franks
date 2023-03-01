5790a6cb1717019d32fee7730451b59249ff863a
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-01 11:49:18 +0800

    > sim RTL 22051115 陈卓 Linux franks-virtual-machine 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:49:18 up 11:10,  1 user,  load average: 0.29, 0.38, 0.35

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vexample.h"
+
+#define MAX_SIM_TIME 20
+vluint64_t sim_time = 0;
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+	Vexample *swch = new Vexample;
+
+	Verilated::traceEverOn(true);
+	VerilatedVcdC *wv = new VerilatedVcdC;
+
+	swch->trace(wv,0);
+	wv->open("wave.vcd");
+
+	while((int)sim_time < MAX_SIM_TIME && !Verilated::gotFinish()){
+		int a = rand() & 1;
+		int b = rand() & 1;
+		swch->a = a;
+		swch->b = b;
+		swch->eval();
+		wv->dump(sim_time);
+		sim_time++;
+	}
+	swch->final();
+	wv->close();
+	delete swch;
+	
+	return 0;
 }
