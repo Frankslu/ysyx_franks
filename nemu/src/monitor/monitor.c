e1774cd525b0ff1e2f2cf66a788e1a5eeda0b74b
22051115-陈卓 <1653954675@qq.com>
2023-03-31 19:06:05 +0800

    NJU-ProjectN/nemu ysyx2204 initialized
    NJU-ProjectN/nemu e59f676a116a48481adc05f584849c43c4e8b41d Merge pull request #54 from silver-ymz/fix-macos-macro-conflict

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -31,10 +31,9 @@ static void welcome() {
         "If it is not necessary, you can disable it in menuconfig"));
   Log("Build time: %s, %s", __TIME__, __DATE__);
   printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
-  printf("For help, type \"help\"\n");/*
+  printf("For help, type \"help\"\n");
   Log("Exercise: Please remove me in the source code and compile NEMU again.");
   assert(0);
-  */
 }
 
 #ifndef CONFIG_TARGET_AM
@@ -78,14 +77,14 @@ static int parse_args(int argc, char *argv[]) {
     {"help"     , no_argument      , NULL, 'h'},
     {0          , 0                , NULL,  0 },
   };
-  volatile int o;
+  int o;
   while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 1: img_file = optarg; return 1;
+      case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
@@ -103,8 +102,7 @@ void init_monitor(int argc, char *argv[]) {
   /* Perform some global initialization. */
 
   /* Parse arguments. */
-  int i=parse_args(argc, argv);
-	printf("%d\n",i);
+  parse_args(argc, argv);
 
   /* Set random seed. */
   init_rand();
