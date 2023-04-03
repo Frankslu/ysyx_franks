2e8b5f987fce32659d5787b9023d9390451779f5
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 21:01:09 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:01:09 up 2 days, 19:45,  1 user,  load average: 0.27, 0.31, 0.27

--- nemu/tools/qemu-diff/include/isa.h
+++ nemu/tools/qemu-diff/include/isa.h
@@ -32,7 +32,7 @@
 
 #elif defined(CONFIG_ISA_loongarch32r)
 #define ISA_QEMU_BIN "qemu-system-loongarch32"
-#define ISA_QEMU_ARGS "-machine" ,"ls3a5k32"
+#define ISA_QEMU_ARGS "-machine","ls3a5k32"
 #else
 #error Unsupport ISA
 #endif
