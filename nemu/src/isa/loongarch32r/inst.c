f2764a87ba841140c6f0e0fc1b71f9e9be111693
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 01:26:13 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:26:13 up  9:12,  1 user,  load average: 0.16, 0.19, 0.17

--- nemu/src/isa/loongarch32r/inst.c
+++ nemu/src/isa/loongarch32r/inst.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include "local-include/reg.h"
 #include <cpu/cpu.h>
@@ -23,8 +23,8 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_2RI12, TYPE_1RI20, TYPE_3R, TYPE_OFFS26, TYPE_2RO16,	
-  TYPE_N, // none
+	TYPE_2RI12, TYPE_1RI20, TYPE_3R, TYPE_OFFS26, TYPE_2RO16,	
+	TYPE_N, // none
 };
 
 #define src1R()  do { *src1 = R(rj); } while (0)
@@ -35,51 +35,53 @@ enum {
 #define offs16() do { *imm = SEXT(BITS(i, 25, 10), 16) << 2;} while (0)
 
 static void decode_operand(Decode *s, int *rd_, word_t *src1, word_t *src2, word_t *imm, int type) {
-  uint32_t i = s->isa.inst.val;
-  int rj = BITS(i, 9, 5);
-  int rk = BITS(i, 14, 10);
-  *rd_ = BITS(i, 4, 0);
-  switch (type) {
-    case TYPE_1RI20: simm20(); src1R(); break;
-    case TYPE_2RI12: simm12(); src1R(); break;
-    case TYPE_3R   : src1R();  src2R(); break;
-    case TYPE_OFFS26: offs26();break;
-    case TYPE_2RO16: offs16(); src1R(); break;
-  }
+	uint32_t i = s->isa.inst.val;
+	int rj = BITS(i, 9, 5);
+	int rk = BITS(i, 14, 10);
+	*rd_ = BITS(i, 4, 0);
+	switch (type) {
+		case TYPE_1RI20: simm20(); src1R(); break;
+		case TYPE_2RI12: simm12(); src1R(); break;
+		case TYPE_3R   : src1R();  src2R(); break;
+		case TYPE_OFFS26: offs26();break;
+		case TYPE_2RO16: offs16(); src1R(); break;
+	}
 }
 
 static int decode_exec(Decode *s) {
-  int rd = 0;
-  word_t src1 = 0, src2 = 0, imm = 0;//imm also used as offset
-  s->dnpc = s->snpc;
+	int rd = 0;
+	word_t src1 = 0, src2 = 0, imm = 0;//imm also used as offset
+	s->dnpc = s->snpc;
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
-  decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
-  __VA_ARGS__ ; \
+	decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
+	__VA_ARGS__ ; \
 }
 
-  INSTPAT_START();
-  INSTPAT("010101 ???????????????? ??????????"    , bl       , OFFS26, s->dnpc = s->pc + imm, R(1) = s->snpc, printf("bl, s->pc=%08x, imm=%08x, s->dnpc=%08x\n",s->pc,imm,s->dnpc));
-  INSTPAT("010011 ???????????????? ????? ?????"   , jirl     , 2RO16 , s->dnpc = src1 + imm, R(rd) = s->snpc, printf("jirl, src1=%08x, imm=%08x, s->dnpc=%08x\n", src1, imm, s->dnpc));
-  INSTPAT("0001110 ????? ????? ????? ????? ?????" , pcaddu12i, 1RI20 , R(rd) = s->pc + imm, printf("pcaddui12i, s->pc=%08x, imm=%08x, R(rd)=%08x\n", s->pc, imm, R(rd)));
-  INSTPAT("0010100010 ???????????? ????? ?????"   , ld.w     , 2RI12 , R(rd) = Mr(src1 + imm, 4), printf("ld.w, s->pc=%08x, imm=%08x, addr=%08x, ,R(rd)=%08x\n", s->pc, imm, s->pc + imm, R(rd)));
-  INSTPAT("0010100110 ???????????? ????? ?????"   , st.w     , 2RI12 , Mw(src1 + imm, 4, R(rd)), printf("st.w, src1=%08x, imm=%08x, addr=%08x, R(rd)=%08x\n", src1,  imm, s->pc + imm, R(rd)));
-  INSTPAT("0000001010 ???????????? ????? ?????"   , addi.w   , 2RI12 , R(rd) = imm + src1, printf("addi.w, src1=%08x, imm=%08x, R(rd)=%08x\n", src1, imm, R(rd)));
-  INSTPAT("00000000000100000 ????? ????? ?????"   , add.w    , 3R    , R(rd) = src1 + src2, printf("add.w, src1=%08x, src2=%08x, R(rd)=%08x\n", src1, src2, R(rd)));
-  INSTPAT("00000000000100010 ????? ????? ?????"   , sub.w    , 3R    , R(rd) = src1 - src2, printf("sub.w, src1=%08x, src2=%08x, R(rd)=%08x\n", src1, src2, R(rd)));
-  INSTPAT("00000000000101010 ????? ????? ?????"   , move(or) , 3R    , R(rd) = src1 | src2, printf("or, src1=%08x, src2=%08x, R(rd)=%08x\n", src1, src2, R(rd)));
-  INSTPAT("00000000000101011 ????? ????? ?????"   , xor      , 3R    , R(rd) = src1 ^ src2, printf("xor, src1=%08x, src2=%08x, R(rd)=%08x\n", src1, src2, R(rd)));
-  INSTPAT("0000 0000 0010 10100 ????? ????? ?????", break    , N     , NEMUTRAP(s->pc, R(4)), printf("break\n")); // R(4) is $a0
-  INSTPAT("????????????????? ????? ????? ?????"   , inv      , N     , INV(s->pc));
-  INSTPAT_END();
+	INSTPAT_START();
+	INSTPAT("010101 ???????????????? ??????????"    , bl       , OFFS26, s->dnpc = s->pc + imm, R(1) = s->snpc, printf("bl, s->pc=%08x, imm=%08x, s->dnpc=%08x\n",s->pc,imm,s->dnpc));
+	INSTPAT("010011 ???????????????? ????? ?????"   , jirl     , 2RO16 , s->dnpc = src1 + imm, R(rd) = s->snpc, printf("jirl, src1=%08x, imm=%08x, s->dnpc=%08x\n", src1, imm, s->dnpc));
+	INSTPAT("0001110 ????? ????? ????? ????? ?????" , pcaddu12i, 1RI20 , R(rd) = s->pc + imm, printf("pcaddui12i, s->pc=%08x, imm=%08x, R(rd)=%08x\n", s->pc, imm, R(rd)));
+	INSTPAT("0010100010 ???????????? ????? ?????"   , ld.w     , 2RI12 , R(rd) = Mr(src1 + imm, 4), printf("ld.w, s->pc=%08x, imm=%08x, addr=%08x, ,R(rd)=%08x\n", s->pc, imm, s->pc + imm, R(rd)));
+	INSTPAT("0010100110 ???????????? ????? ?????"   , st.w     , 2RI12 , Mw(src1 + imm, 4, R(rd)), printf("st.w, src1=%08x, imm=%08x, addr=%08x, R(rd)=%08x\n", src1,  imm, s->pc + imm, R(rd)));
+	INSTPAT("0000001010 ???????????? ????? ?????"   , addi.w   , 2RI12 , R(rd) = imm + src1, printf("addi.w, src1=%08x, imm=%08x, R(rd)=%08x\n", src1, imm, R(rd)));
+	INSTPAT("0000001000 ???????????? ????? ?????"   , slti     , 2RI12 , R(rd) = (signed)src1 < (signed)imm ? 1 : 0, printf("addi.w, src1=%08x, imm=%08x, R(rd)=%08x\n", src1, imm, R(rd)));
+	INSTPAT("0000001000 ???????????? ????? ?????"   , sltui    , 2RI12 , R(rd) = (unsigned)src1 < (unsigned)imm ? 1 : 0, printf("addi.w, src1=%08x, imm=%08x, R(rd)=%08x\n", src1, imm, R(rd)));
+	INSTPAT("00000000000100000 ????? ????? ?????"   , add.w    , 3R    , R(rd) = src1 + src2, printf("add.w, src1=%08x, src2=%08x, R(rd)=%08x\n", src1, src2, R(rd)));
+	INSTPAT("00000000000100010 ????? ????? ?????"   , sub.w    , 3R    , R(rd) = src1 - src2, printf("sub.w, src1=%08x, src2=%08x, R(rd)=%08x\n", src1, src2, R(rd)));
+	INSTPAT("00000000000101010 ????? ????? ?????"   , move(or) , 3R    , R(rd) = src1 | src2, printf("or, src1=%08x, src2=%08x, R(rd)=%08x\n", src1, src2, R(rd)));
+	INSTPAT("00000000000101011 ????? ????? ?????"   , xor      , 3R    , R(rd) = src1 ^ src2, printf("xor, src1=%08x, src2=%08x, R(rd)=%08x\n", src1, src2, R(rd)));
+	INSTPAT("0000 0000 0010 10100 ????? ????? ?????", break    , N     , NEMUTRAP(s->pc, R(4)), printf("break\n")); // R(4) is $a0
+	INSTPAT("????????????????? ????? ????? ?????"   , inv      , N     , INV(s->pc));
+	INSTPAT_END();
 
-  R(0) = 0; // reset $zero to 0
+	R(0) = 0; // reset $zero to 0
 
-  return 0;
-}
+	return 0;
+	}
 
 int isa_exec_once(Decode *s) {
-  s->isa.inst.val = inst_fetch(&s->snpc, 4);
-  return decode_exec(s);
+	s->isa.inst.val = inst_fetch(&s->snpc, 4);
+	return decode_exec(s);
 }
