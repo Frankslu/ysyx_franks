2800cbbc2c4f4c431c8bd4af798e7edf8ed760fc
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 12:32:34 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:32:34 up 4 days, 11:16,  1 user,  load average: 0.35, 0.39, 0.30

--- nemu/tools/qemu-diff/src/gdb-host.c
+++ nemu/tools/qemu-diff/src/gdb-host.c
@@ -58,6 +58,7 @@ bool gdb_memcpy_to_qemu(uint32_t dest, void *src, int len) {
   ok &= gdb_memcpy_to_qemu_small(dest, src, len);
   char *buf="m1c000000";
   gdb_send(conn, (const uint8_t *)buf, strlen(buf));
+  size_t size;
   uint8_t *reply = gdb_recv(conn, &size);
   printf("%s\n",(char *)reply);
   
