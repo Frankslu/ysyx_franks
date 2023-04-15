41aa03101941b1de69ddb0097e7be3f0f5e13e17
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 02:24:54 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:24:54 up  4:08,  1 user,  load average: 13.03, 12.61, 12.25

--- npc/playground/src/cpucore/pipeline/EX_stage.scala
+++ npc/playground/src/cpucore/pipeline/EX_stage.scala
@@ -4,9 +4,7 @@ import cpucore.Config.Configs
 class EX_stage extends Module{
     val io = IO(new Bundle {
         val a = Input(UInt(1.W))
-        val b = Output(UInt(1.W))
     })
 
     val reg = RegNext(io)
-    io.b := 1.U
 }
