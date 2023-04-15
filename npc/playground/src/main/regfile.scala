35b198aeb0cbf408ec09cc0170dafe73b8a2afe4
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 00:51:03 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:51:03 up  2:35,  1 user,  load average: 10.23, 9.80, 8.93

--- npc/playground/src/main/regfile.scala
+++ npc/playground/src/main/regfile.scala
@@ -2,7 +2,7 @@ import chisel3._
 import config.Configs._
 
 class regfile extends Module{
-    val io = new Bundle {
+    val io = IO(new Bundle {
         val waddr = Input(UInt(REG_ADDR_LEN.W))
         val raddr1 = Input(UInt(REG_ADDR_LEN.W))
         val raddr2 = Input(UInt(REG_ADDR_LEN.W))
@@ -10,14 +10,14 @@ class regfile extends Module{
         val rdata1 = Output(UInt(DATA_WIDTH.W))
         val rdata2 = Output(UInt(DATA_WIDTH.W))
         val wen = Input(Bool())
-    }
+    })
 
     val regfile = Reg(Vec(REG_NUM, UInt(DATA_WIDTH.W)))
 
     when(io.wen){
-        regfile(io.waddr.asInstanceOf[Int]) := io.wdata
+        regfile(io.waddr) := io.wdata
     }
 
-    io.rdata1 := Mux(io.raddr1 === 0.U, 0.U, regfile(io.raddr1.asInstanceOf[Int]))
-    io.rdata2 := Mux(io.raddr2 === 0.U, 0.U, regfile(io.raddr2.asInstanceOf[Int]))
+    io.rdata1 := Mux(io.raddr1 === 0.U, 0.U, regfile(io.raddr1))
+    io.rdata2 := Mux(io.raddr2 === 0.U, 0.U, regfile(io.raddr2))
 }
