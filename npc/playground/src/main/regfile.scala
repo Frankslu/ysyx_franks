5701c7741f16f64416f69e779eb8c6c8b0ab51f9
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 00:46:43 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:46:43 up  2:30,  1 user,  load average: 10.58, 9.81, 8.66

--- npc/playground/src/main/regfile.scala
+++ npc/playground/src/main/regfile.scala
@@ -9,12 +9,15 @@ class regfile extends Module{
         val wdata = Input(UInt(DATA_WIDTH.W))
         val rdata1 = Output(UInt(DATA_WIDTH.W))
         val rdata2 = Output(UInt(DATA_WIDTH.W))
+        val wen = Input(Bool())
     }
 
     val regfile = Reg(Vec(REG_NUM, UInt(DATA_WIDTH.W)))
 
-    regfile(io.waddr) := io.wdata
+    when(io.wen){
+        regfile(io.waddr.asInstanceOf[Int]) := io.wdata
+    }
 
-    io.rdata1 := Mux(io.raddr1 === 0.U, 0.U, regfile(io.raddr1))
-    io.rdata2 := Mux(io.raddr2 === 0.U, 0.U, regfile(io.raddr2))
+    io.rdata1 := Mux(io.raddr1 === 0.U, 0.U, regfile(io.raddr1.asInstanceOf[Int]))
+    io.rdata2 := Mux(io.raddr2 === 0.U, 0.U, regfile(io.raddr2.asInstanceOf[Int]))
 }
