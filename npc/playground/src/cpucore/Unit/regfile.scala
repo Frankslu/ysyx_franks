1cdb9bf1b91f99288faf8ea9649765b8a069aad9
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 00:51:51 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:51:51 up  2:32,  1 user,  load average: 13.05, 13.36, 12.83

--- npc/playground/src/cpucore/Unit/regfile.scala
+++ npc/playground/src/cpucore/Unit/regfile.scala
@@ -24,19 +24,17 @@ class regfile extends Module{
     io1.rdata2 := Mux(io1.raddr2 === 0.U, 0.U, regfile(io1.raddr2))
 }*/
 class regfile extends Module{
-    val io1 = IO(new Bundle {
+    val io = IO(new Bundle {
         val raddr1 = Input(UInt(REG_ADDR_LEN.W))
         val raddr2 = Input(UInt(REG_ADDR_LEN.W))
         val rdata1 = Output(UInt(DATA_WIDTH.W))
         val rdata2 = Output(UInt(DATA_WIDTH.W))
-    })
-    val io2 = IO(new Bundle {
         val waddr = Input(UInt(REG_ADDR_LEN.W))
         val wdata = Input(UInt(DATA_WIDTH.W))
         val wen = Input(Bool())
     })
     val rf = Mem(REG_NUM ,UInt(DATA_WIDTH.W))
-    when(io2.wen) {rf(io2.waddr) := io2.wdata}
-    io1.rdata1 := Mux(io1.raddr1 === 0.U, 0.U, rf(io1.raddr1))
-    io1.rdata2 := Mux(io1.raddr2 === 0.U, 0.U, rf(io1.raddr2))
+    when(io.wen) {rf(io.waddr) := io.wdata}
+    io.rdata1 := Mux(io.raddr1 === 0.U, 0.U, rf(io.raddr1))
+    io.rdata2 := Mux(io.raddr2 === 0.U, 0.U, rf(io.raddr2))
 }
--- npc/playground/src/cpucore/pipeline/ID_stage.scala
+++ npc/playground/src/cpucore/pipeline/ID_stage.scala
@@ -11,6 +11,8 @@ class ID_stage extends Module{
     val toes = IO(new ds2es)
     val tofs = IO(Flipped(new fs2ds))
     val br   = IO(Flipped(new br_bus))
+    br.taken := true.B
+    br.target := 0.U
 
     val inst = tofs.inst
     val rj = inst(9,5)
@@ -22,11 +24,14 @@ class ID_stage extends Module{
     val imm26 = Cat(rj, rd, inst(25,10))
 
     val reg = Module(new regfile)
-    reg.io1.raddr1 := rj
-    reg.io1.raddr2 := rk
+    reg.io.raddr1 := rj
+    reg.io.raddr2 := rk
+    reg.io.waddr := 0.U
+    reg.io.wdata := 0.U
+    reg.io.wen := 1.U
 
-    val rj_value = reg.io1.rdata1
-    val rk_value = reg.io1.rdata2
+    val rj_value = reg.io.rdata1
+    val rk_value = reg.io.rdata2
 
     toes.alu_op := loongarch32r_inst.loongarch32r_decoder(inst)
     toes.alu_src1 := rj_value
