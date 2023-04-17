ace30e74902c927b59c3d648c0ce871182402e0c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 17:10:09 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:09 up  2:11,  1 user,  load average: 4.71, 4.51, 3.18

--- npc/playground/src/cpucore/pipeline/te.scala
+++ npc/playground/src/cpucore/pipeline/te.scala
@@ -14,8 +14,10 @@ class test extends Module{
     val rk = inst(14,10)
     val imm5  = rk
     val imm12u = inst(21,10)
-    val imm12  = inst(21,10).asSInt
-    val imm16  = Cat(inst(25,10),0.U(2.W)).asSInt
+    val imm12  = Wire(SInt(32.W))
+    imm12 := inst(21,10).asSInt
+    val imm16  = Wire(SInt(32.W))
+    imm16 := Cat(inst(25,10),0.U(2.W)).asSInt
     val imm20  = Cat(inst(24,5), 0.U(12.W))
     val imm26  = Cat(rj, rd, inst(25,10))
 
@@ -23,7 +25,7 @@ class test extends Module{
         (inst_type === s"b${R2I5}".U)  -> imm5,
         (inst_type === s"b${R2I12}".U) -> imm12.asUInt,
         (inst_type === s"b${R2I12U}".U) -> imm12u,
-        (inst_type === s"b${R2I16}".U) -> imm16.asUInt,
+        (inst_type === s"b${R2I16}".U) -> imm16.asSInt,
         (inst_type === s"b${R1I20}".U) -> imm20,
         (inst_type === s"b${I26}".U)   -> imm26
     ))
