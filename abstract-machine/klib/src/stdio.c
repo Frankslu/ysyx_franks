2399fa5126f1c80caaea25b80cad643186911e19
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-23 15:54:16 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:54:16 up  4:02,  1 user,  load average: 0.66, 0.53, 0.44

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,37 +5,53 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-// void put_int(int d){
+void int_to_char(int d, char s[]);
 
-// }
+int printf(const char *fmt, ...) {
+	if (fmt == NULL){
+		return -1;
+	}
 
-// int printf(const char *fmt, ...) {
-// 	if (fmt == NULL){
-// 		return -1;
-// 	}
+	va_list args;
+	va_start(args, fmt);
+	int len = 0;
+	int d;
+	char c;
+	char s1[33];
+	char *s;
 
-// 	va_list args;
-// 	va_start(args, fmt);
-// 	int d;
-// 	char c;
-// 	char *s = ;
+	size_t i = 0;
+	while (fmt[i] != '\0'){
+		if (fmt[i] == '%'){
+			i++;
+			switch (fmt[i]){
+				case 'd':
+					d = va_arg(args, int);
+					int_to_char(d, s1);
+					putstr(s1);
+					len += strlen(s1);
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					putstr(s);
+					len += strlen(s);
+					break;
+				case 'c':
+					c = (char)va_arg(args, int);
+					putch(c);
+					len++;
+					break;
+			}
+		}
+		else {
+			putch(fmt[i]);
+			len++;
+		}
+		i++;
+	}
 
-// 	size_t i = 0;
-// 	while (fmt[i] != '\0'){
-// 		if (fmt[i] == '%'){
-// 			i++;
-// 			switch (fmt[i]){
-// 				case 'd':
-// 					d = va_arg(args, int);
-// 					put_int(d);
-// 			}
-// 		}
-// 		else {
-// 			putch(fmt[i]);
-// 		}
-// 		i++;
-// 	}
-// }
+	return len;
+}
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
 	panic("Not implemented");
@@ -51,6 +67,25 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 	panic("Not implemented");
+}
+
+void int_to_char(int d, char s[]){
+	char tmp;
+	int i = 0;
+
+	while (d != 0){
+		s[i] = (char)(d % 10) - '0';
+		d /= 10;
+		i++;
+	}
+
+	s[i] = '\0';
+
+	for (int j=0; j < (i-1-j); j++){
+		tmp = s[i];
+		s[i] = s[i-1-j];
+		s[i-1-j] = tmp;
+	}
 }
 
 #endif
