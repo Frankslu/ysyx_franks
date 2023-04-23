08f8d94ffe38d4973708c1873bccb8c51b74f081
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-23 14:08:21 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:08:21 up  2:16,  1 user,  load average: 0.27, 0.24, 0.23

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,52 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
+// void put_int(int d){
+
+// }
+
+// int printf(const char *fmt, ...) {
+// 	if (fmt == NULL){
+// 		return -1;
+// 	}
+
+// 	va_list args;
+// 	va_start(args, fmt);
+// 	int d;
+// 	char c;
+// 	char *s = ;
+
+// 	size_t i = 0;
+// 	while (fmt[i] != '\0'){
+// 		if (fmt[i] == '%'){
+// 			i++;
+// 			switch (fmt[i]){
+// 				case 'd':
+// 					d = va_arg(args, int);
+// 					put_int(d);
+// 			}
+// 		}
+// 		else {
+// 			putch(fmt[i]);
+// 		}
+// 		i++;
+// 	}
+// }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 #endif
