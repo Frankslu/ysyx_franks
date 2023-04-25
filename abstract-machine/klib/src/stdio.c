b256737bf75cf76eb773fed06b14c91b4d1877b7
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-26 00:18:15 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:18:15 up 51 min,  1 user,  load average: 0.81, 0.59, 0.46

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -63,7 +63,104 @@ int sprintf(char *out, const char *fmt, ...) {
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-	panic("Not implemented");
+	if (fmt == NULL){
+		return -1;
+	}
+
+	va_list args;
+	va_start(args, fmt);
+
+	int len = 0;
+	int arg_len;
+	int d;
+	char c;
+	char s1[33];
+	char *s;
+
+	size_t i = 0;
+	while (fmt[i] != '\0'){
+		if (fmt[i] == '%'){
+			i++;
+			switch (fmt[i]){
+				case 'd':
+					d = va_arg(args, int);
+					itoa(s1, d, 10);
+					arg_len = strlen(s1);
+					if (arg_len + len < n - 1){
+						memcpy(out + len, s1, strlen(s1));
+					}
+					else {
+						memcpy(out + len, s1, n - 1 - len);
+						len += strlen(s1);
+						goto L1;
+					}
+					len += strlen(s1);
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					arg_len = strlen(s);
+					if (arg_len + len < n - 1){
+						memcpy(out + len, s, strlen(s));
+					}
+					else {
+						memcpy(out + len, s, n - 1 - len);
+						len += strlen(s);
+						goto L1;
+					}
+					len += strlen(s);
+					break;
+				case 'c':
+					c = (char)va_arg(args, int);
+					out[len] = c;
+					len++;
+					if (len == n - 1){
+						goto L1;
+					}
+					break;
+			}
+		}
+		else {
+			out[len] = fmt[i];
+			len++;
+			if (len == n){
+				goto L1;
+			}
+		}
+		i++;
+	}
+	out[len] = '\0';
+
+	return len;
+
+	L1:
+	i++;
+	out[n-1] = '\0';
+	while (fmt[i] != '\0'){
+		if (fmt[i] == '%'){
+			i++;
+			switch (fmt[i]){
+				case 'd':
+					d = va_arg(args, int);
+					itoa(s1, d, 10);
+					len += strlen(s1);
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					len += strlen(s);
+					break;
+				case 'c':
+					len++;
+					break;
+			}
+		}
+		else {
+			len++;
+		}
+		i++;
+	}
+
+	return len;
+
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
