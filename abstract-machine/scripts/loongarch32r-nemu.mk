a83d1777ad045cd44f7ef62060e4891d47e005d9
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-02 00:52:09 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:52:09 up 1 day, 12:36,  1 user,  load average: 0.36, 0.48, 0.47

--- abstract-machine/scripts/loongarch32r-nemu.mk
+++ abstract-machine/scripts/loongarch32r-nemu.mk
@@ -1,8 +1,7 @@
-$(info second)
 include $(AM_HOME)/scripts/isa/loongarch32r.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
 CFLAGS  += -DISA_H=\"loongarch/loongarch32r.h\"
-
+$(info second)
 AM_SRCS += loongarch/nemu/start.S \
            loongarch/nemu/cte.c \
            loongarch/nemu/trap.S \
