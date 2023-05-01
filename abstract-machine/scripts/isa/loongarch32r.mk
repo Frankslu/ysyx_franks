2f3869b637f81c98a8ddcaa8b0d1efcbfd3a9220
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-02 00:51:31 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:51:31 up 1 day, 12:35,  1 user,  load average: 0.52, 0.52, 0.49

--- abstract-machine/scripts/isa/loongarch32r.mk
+++ abstract-machine/scripts/isa/loongarch32r.mk
@@ -2,3 +2,4 @@ CROSS_COMPILE := loongarch32r-linux-gnusf-
 COMMON_FLAGS  := -fno-pic
 CFLAGS        += $(COMMON_FLAGS) -static
 ASFLAGS       += $(COMMON_FLAGS) -O0
+$(info thrid)
--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -6,7 +6,7 @@ AM_SRCS := platform/nemu/trm.c \
            platform/nemu/ioe/audio.c \
            platform/nemu/ioe/disk.c \
            platform/nemu/mpe.c
-
+$(info forth)
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x1C000000 --defsym=_entry_offset=0x0
