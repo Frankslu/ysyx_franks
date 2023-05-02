f4eee2335dd81a3dc7a4474518eac40f1f256591
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-03 00:05:36 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:05:36 up 10:17,  1 user,  load average: 0.43, 0.40, 0.38

--- nemu/scripts/config.mk
+++ nemu/scripts/config.mk
@@ -30,7 +30,7 @@ silent := -s
 
 CONF   := $(KCONFIG_PATH)/build/conf
 MCONF  := $(KCONFIG_PATH)/build/mconf
-FIXDEP := $(FIXDEP_PATH)/build/ffffff
+FIXDEP := $(FIXDEP_PATH)/build
 
 $(CONF):
 	$(Q)$(MAKE) $(silent) -C $(KCONFIG_PATH) NAME=conf
