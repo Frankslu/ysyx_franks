6d0171c80d28eebc5dd6ea695a5adc50a1ed4d50
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 04:36:03 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:36:03 up  6:31,  1 user,  load average: 1.31, 0.64, 0.45

--- nemu/src/trace/trace.c
+++ nemu/src/trace/trace.c
@@ -15,41 +15,41 @@ void iring_init();
 void mring_init();
 
 void init_trace(){
-    IFDEF(CONFIG_IRING, iring_init());
-    IFDEF(CONFIG_MTRACE, mring_init());
+	IFDEF(CONFIG_IRING, iring_init());
+	IFDEF(CONFIG_MTRACE, mring_init());
 }
 
 void iring_init(){
-    for (int i=0; i<IRING_BUFSIZE; i++){
-        iring.buf[i][0] = '\0';
-    }
-    iring.pos = 0;
+	for (int i=0; i<IRING_BUFSIZE; i++){
+		iring.buf[i][0] = '\0';
+	}
+	iring.pos = 0;
 }
 
 void mring_init(){
-    for (int i=0; i<MRING_BUFSIZE; i++){
-        mring.pc[i] = 0;
-        mring.addr[i] = 0;
-        mring.wr[i] = INVALID;
-    }
-    mring.pos = 0;
+	for (int i=0; i<MRING_BUFSIZE; i++){
+		mring.pc[i] = 0;
+		mring.addr[i] = 0;
+		mring.wr[i] = INVALID;
+	}
+	mring.pos = 0;
 }
 
 
 #ifdef CONFIG_IRING
 void iring_write(char *_buf){
-    strcpy(iring.buf[iring.pos], _buf);
-    iring.pos = iring.pos == (IRING_BUFSIZE - 1) ? 0 : (iring.pos + 1);
+	strcpy(iring.buf[iring.pos], _buf);
+	iring.pos = iring.pos == (IRING_BUFSIZE - 1) ? 0 : (iring.pos + 1);
 }
 
 void display_iring(){
-    
-    for (int i=0; i<IRING_BUFSIZE; i++){
-        if (iring.buf[iring.pos][0] != '\0'){
-            printf("%s\n", iring.buf[iring.pos]);
-        }
-        iring.pos = iring.pos == (IRING_BUFSIZE - 1) ? 0 : (iring.pos + 1);
-    }
+	
+	for (int i=0; i<IRING_BUFSIZE; i++){
+		if (iring.buf[iring.pos][0] != '\0'){
+			printf("%s\n", iring.buf[iring.pos]);
+		}
+		iring.pos = iring.pos == (IRING_BUFSIZE - 1) ? 0 : (iring.pos + 1);
+	}
 }
 #endif
 
@@ -57,84 +57,91 @@ void display_iring(){
 
 #ifdef CONFIG_MTRACE
 void record_read(vaddr_t addr){
-    if(addr < CONFIG_MTRACE_START || addr > CONFIG_MTRACE_END)
-        return;
-    mring.pc[mring.pos] = cpu.pc;
-    mring.addr[mring.pos] = addr;
-    mring.wr[mring.pos] = READ;
-    mring.pos = mring.pos == (MRING_BUFSIZE - 1) ? 0 : (mring.pos + 1);
+	if(addr < CONFIG_MTRACE_START || addr > CONFIG_MTRACE_END)
+		return;
+	mring.pc[mring.pos] = cpu.pc;
+	mring.addr[mring.pos] = addr;
+	mring.wr[mring.pos] = READ;
+	mring.pos = mring.pos == (MRING_BUFSIZE - 1) ? 0 : (mring.pos + 1);
 }
 
 void record_write(vaddr_t addr){
-    if(addr < CONFIG_MTRACE_START || addr > CONFIG_MTRACE_END)
-        return;
-    mring.pc[mring.pos] = cpu.pc;
-    mring.addr[mring.pos] = addr;
-    mring.wr[mring.pos] = WRITE;
-    mring.pos = mring.pos == (MRING_BUFSIZE - 1) ? 0 : (mring.pos + 1);
+	if(addr < CONFIG_MTRACE_START || addr > CONFIG_MTRACE_END)
+		return;
+	mring.pc[mring.pos] = cpu.pc;
+	mring.addr[mring.pos] = addr;
+	mring.wr[mring.pos] = WRITE;
+	mring.pos = mring.pos == (MRING_BUFSIZE - 1) ? 0 : (mring.pos + 1);
 }
 
 void display_mring(){
-    for (int i=0; i<MRING_BUFSIZE; i++){
-        if (mring.wr[mring.pos] != INVALID){
-            printf("%x  %s %x\n", mring.pc[mring.pos], mring.wr[mring.pos] == READ ? "read" : "write", mring.addr[mring.pos]);
-        }
-        mring.pos = mring.pos == (MRING_BUFSIZE - 1) ? 0 : (mring.pos + 1);
-    }
+	for (int i=0; i<MRING_BUFSIZE; i++){
+		if (mring.wr[mring.pos] != INVALID){
+			printf("%x  %s %x\n", mring.pc[mring.pos], mring.wr[mring.pos] == READ ? "read" : "write", mring.addr[mring.pos]);
+		}
+		mring.pos = mring.pos == (MRING_BUFSIZE - 1) ? 0 : (mring.pos + 1);
+	}
 }
 #endif
 
 
-// #ifdef CONFIG_FTRACE
-// void init_ftrace(char elf_file){
-//     if (elf_file == NULL){
-//         Log("no input elf file");
-//         return;
-//     }
+#ifdef CONFIG_FTRACE
+void init_ftrace(char *elf_file){
+typedef MUXDEF(CONFIG_ISA64, Elf64_Shdr, Elf32_Shdr) Elf_Shdr;
+typedef MUXDEF(CONFIG_ISA64, Elf64_Ehdr, Elf32_Ehdr) Elf_Ehdr;
+typedef MUXDEF(CONFIG_ISA64, Elf64_Sym , Elf32_Sym ) Elf_Sym;
+	int res;
+	if(elf_file == NULL)
+		return;
 
-//     int count;
-//     FILE *fp;
-//     fp = fopen(elf_file, "rb");
-//     Assert(fp, "Can not open %s", elf_file);
+	FILE *fp = fopen(elf_file, "rb");
+	Assert(fp, "Can not open %s\n", elf_file);
 
-//     char *buf;
-//     buf = (char *)malloc(9192 * sizeof(unsigned char));
-//     int ret = fread(buf, sizeof(unsigned char), 9192, fp);
-//     assert(ret == 1);
+	Elf_Ehdr section_header;
+	res = fread(&section_header, sizeof(section_header), 1, fp);
 
-//     Elf32_Ehdr *ehdr = (Elf32_Ehdr *)buf;
-//     Elf32_Shdr *shdr = (Elf32_Shdr *)(buf + ehdr->e_shoff);
-//     Elf32_Shdr *shdr_strtab = NULL;
-//     Elf32_Shdr *shdr_symtab = NULL;
+	Assert(section_header.e_ident[EI_MAG0] != ELFMAG0 ||
+		section_header.e_ident[EI_MAG1] != ELFMAG1 ||
+		section_header.e_ident[EI_MAG2] != ELFMAG2 ||
+		section_header.e_ident[EI_MAG3] != ELFMAG3,
+		"%s is not elf\n", elf_file);
 
-//     for (int i = 0; shdr_strtab == NULL || shdr_symtab == NULL; i++)
-//     {
-//         if (shdr[i].sh_type == SHT_SYMTAB)
-//         {
-//             shdr_symtab = &shdr[i];
-//         }
-//         else if (shdr[i].sh_type == SHT_STRTAB)
-//         {
-//             shdr_strtab = &shdr[i];
-//             break;
-//         }
-//     }
+	Elf_Shdr section_table[section_header.e_shnum];
+	fseek(fp, section_header.e_shoff, SEEK_SET);
+	res = fread(section_table, sizeof(section_table), 1, fp);
 
-//     Elf32_Sym *table_sym = (Elf32_Sym *)(buf + shdr_symtab->sh_offset);
 
-//     for (int i = 0; i <= shdr_symtab->sh_size / shdr_symtab->sh_entsize; i++)
-//     {
-//         if (ELF64_ST_TYPE(table_sym[i].st_info) == STT_FUNC)
-//         {
-//             func[count].addr = table_sym[i].st_value;
-//             strcpy(func[count].name, (char *)(buf + shdr_strtab->sh_offset + table_sym[i].st_name));
-//             count++;
-//         }
-//     }
-
-//     free(buf);
-//     return;
-// }
-// #endif
+	Elf_Shdr *symtab = NULL, *strtab = NULL;
+	int a = 0;
+	for (int i = 0; i < section_header.e_shnum; i++) {
+		if (section_table[i].sh_type == SHT_SYMTAB) {
+			symtab = &section_table[i];
+			a++;
+		}
+		if (section_table[i].sh_type == SHT_STRTAB) {
+			strtab = &section_table[i];
+			a++;
+		}
+		if (a == 2){
+			break;
+		}
+	} 
+	
+	for (int i=0; i < symtab->sh_size / sizeof(Elf_Sym); i++) {
+		char name[50];
+		Elf_Sym sym;
+		fseek(fp, symtab->sh_offset + i * sizeof(sym), SEEK_SET);
+		res = fread(&sym, sizeof(sym), 1, fp);
+		fseek(fp, strtab->sh_offset + sym.st_name, SEEK_SET);
+		res = fread(name, 50, 1, fp);
+		if (ELF32_ST_TYPE(sym.st_info) == STT_FUNC) {
+			printf("Function %s at address %x, %d\n", name, sym.st_value, sym.st_name);
+		}
+	}
+	fclose(fp);
+	res++;
+	return;
+}
+#endif
 
 #endif
