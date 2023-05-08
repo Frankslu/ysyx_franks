ef556bcd132e9b7c32f56b83cb42357e65d5a01b
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-08 12:43:58 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:43:58 up 31 min,  1 user,  load average: 0.06, 0.07, 0.08

--- npc/playground/src/cpucore/pipeline/interface.scala
+++ npc/playground/src/cpucore/pipeline/interface.scala
@@ -32,7 +32,7 @@ class es2ms extends Bundle{
 
 class sram_io_1 extends Bundle{
     val en = Output(Bool())
-    val we = Output(UInt(WSTRB_WIDTH.W))
+    val wr = Output(Bool())
     val addr = Output(UInt(ADDR_WIDTH.W))
     val wdata = Output(UInt(DATA_WIDTH.W))
     val wstrb = Output(UInt(WSTRB_WIDTH.W))
--- npc/playground/src/memory/Mem.scala
+++ npc/playground/src/memory/Mem.scala
@@ -6,18 +6,18 @@ import cpucore.pipeline._
 
 class dmem extends BlackBox with HasBlackBoxInline {
 	val io = IO(new Bundle{
-		new sram_io_1
-		new sram_io_2}
+		val r = new sram_io_1
+		val w = new sram_io_2}
 	)
 
 	setInline("memory.v",
 	"""module mem(
 		|	input en,
-		|	input [3:0]   we,
-		|	input [31:0]  addr,
-		|	input [31:0]  wdata,
-		|	input [4:0]   wstrb,
-		|	output [31:0] rdata
+		|	input [3:0]   r_we,
+		|	input [31:0]  r_addr,
+		|	input [31:0]  r_wdata,
+		|	input [4:0]   r_wstrb,
+		|	output [31:0] w_rdata
 		|);
 		|import "DPI-C" function void pmem_read(
 		|	input longint raddr, output longint rdata);
@@ -25,6 +25,8 @@ class dmem extends BlackBox with HasBlackBoxInline {
 		|	input longint waddr, input longint wdata, input byte wmask);
 		|wire [63:0] rdata;
 		|always @(*) begin
+		|	if(en) begin
+		|		if()
 		|	pmem_read(raddr, rdata);
 		|	pmem_write(waddr, wdata, wmask);
 		|end
@@ -33,11 +35,11 @@ class dmem extends BlackBox with HasBlackBoxInline {
 }
 
 class rdmem extends Module {
-	val mem_io = IO(new Bundle{
-		new sram_io_1
-		new sram_io_2
-	})
 
 	val mem = Module(new dmem)
+	val mem_io = IO(new Bundle{
+		val r = new sram_io_1
+		val w = new sram_io_2
+	})
 	mem.io <> mem_io
 }
