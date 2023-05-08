12fd697a4a9f3954859503a2de79c02ce4263a87
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-09 02:41:35 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:41:35 up  3:31,  1 user,  load average: 0.82, 0.58, 0.56

--- npc/src/src/memory/Mem1.scala
+++ npc/src/src/memory/Mem1.scala
@@ -1,8 +1,8 @@
-package cpucore.memory
+// package cpucore.memory
 
-import chisel3._
-import chisel3.util._
-import cpucore.pipeline._
+// import chisel3._
+// import chisel3.util._
+// import cpucore.pipeline._
 
 // class rdmem extends Module {
 // 	val mem = Module(new mem)
