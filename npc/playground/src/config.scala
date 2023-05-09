30df02112d122c5d73b1c47cac6cd7b4346adce4
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-09 14:45:12 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:45:12 up  2:29,  1 user,  load average: 0.63, 0.46, 0.46

---
+++ npc/playground/src/config.scala
@@ -0,0 +1,16 @@
+package cpucore.Config
+
+object Configs {
+    //Rrgfile
+    val REG_ADDR_LEN = 5
+    val REG_NUM = 32
+
+    //ALU
+    val ALU_OP_NUM = 19
+    val ADDR_WIDTH = 32
+    val DATA_WIDTH = 32
+
+    val PC_INIT = 0x1c000000
+
+    val WSTRB_WIDTH = 4
+}
