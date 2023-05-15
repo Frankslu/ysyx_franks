5b01b4eda716ce1cff652c08c7291eabde7a8b9d
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-16 00:30:19 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:30:19 up  1:27,  1 user,  load average: 0.34, 0.39, 0.35

--- npc-1/src/src/memory/Mem.scala
+++ npc-1/src/src/memory/Mem.scala
@@ -4,45 +4,56 @@ import chisel3._
 import chisel3.util._
 import cpucore.pipeline._
 
+class io1 extends Bundle{
+  val r_en = Input(Bool())
+  val r_wr = Input(Bool())
+  val r_addr = Input(UInt(32.W))
+  val r_wdata = Input(UInt(32.W))
+  val r_wstrb = Input(UInt(4.W))
+  val w_rdata = Output(UInt(32.W))
+}
+
 class mem extends Module {
-    val io = IO(Flipped(new Bundle{
-    val r = new sram_io_1
-    val w = new sram_io_2
-  }))
-    val Memory = Module(new Memory())
-    Memory.io <> io
-    
+  //   val io = IO(Flipped(new Bundle{
+  //   val r = new sram_io_1
+  //   val w = new sram_io_2
+  // }))
+	val io = IO(new io1)
+	val Memory = Module(new Memory())
+	Memory.io <> io
+	
 }
 
 class Memory extends BlackBox with HasBlackBoxInline {
-  val io = IO(Flipped(new Bundle{
-    val r = new sram_io_1
-    val w = new sram_io_2
-  }))
-    setInline("Memory.v",
-    """module Memory(
-	    | input  r_en,
-	    | input  r_wr,
-	    | input  [31:0]  r_addr,
-	    | input  [31:0]  r_wdata,
-	    | input  [3:0]   r_wstrb,
-	    | output [31:0] w_rdata
-      | );
-      | import "DPI-C" function void pmem_read(
-	    | input int raddr, output int rdata);
-      | import "DPI-C" function void pmem_write(
-	    | input int waddr, input int wdata, input byte wmask);
-      | wire [63:0] rdata;
-      | always @(*) begin
-	    |   if(r_en) begin
+//   val io = IO(Flipped(new Bundle{
+// 	val r = new sram_io_1
+// 	val w = new sram_io_2
+//   }))
+	val io = IO(new io1)
+	setInline("Memory.v",
+	"""module Memory(
+		| input  r_en,
+		| input  r_wr,
+		| input  [31:0]  r_addr,
+		| input  [31:0]  r_wdata,
+		| input  [3:0]   r_wstrb,
+		| output [31:0] w_rdata
+	  | );
+	  | import "DPI-C" function void pmem_read(
+		| input int raddr, output int rdata);
+	  | import "DPI-C" function void pmem_write(
+		| input int waddr, input int wdata, input byte wmask);
+	  | wire [63:0] rdata;
+	  | always @(*) begin
+		|   if(r_en) begin
 		  |     if(r_wr) begin
 			|       pmem_read(r_addr, w_rdata);
 		  |     end else begin
 			|       pmem_write(r_addr, r_wdata, r_wstrb);
 		  |     end
-	    |   end
-      |end
-      |
-      |endmodule
-    """.stripMargin)
+		|   end
+	  |end
+	  |
+	  |endmodule
+	""".stripMargin)
 }
