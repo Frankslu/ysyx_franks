6f67a2b6834b385ada13eb3e7faf913f02bc728b
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-16 16:57:12 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:57:12 up  3:40,  1 user,  load average: 0.63, 0.46, 0.31

--- npc/src/src/cpucore/Unit/ALU.scala
+++ npc/src/src/cpucore/Unit/ALU.scala
@@ -2,7 +2,7 @@ package cpucore.Unit
 
 import chisel3._
 import chisel3.util._
-import chisel3.experimental._
+// import chisel3.experimental._
 import circt.stage.ChiselStage
 import cpucore.Config.Configs._
 import myUtil.myUtil._
