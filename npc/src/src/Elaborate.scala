374becf2687c0cf8bd009edc2576d0296a87d99f
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-17 11:29:41 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:29:41 up  1:05,  1 user,  load average: 0.42, 0.68, 0.54

--- npc/src/src/Elaborate.scala
+++ npc/src/src/Elaborate.scala
@@ -8,9 +8,9 @@ import cpucore.Unit._
 // import cpucore.pipeline._
 
 object Elaborate extends App {
-  def top = new ALU()
+  def top = new mem()
   
-  val useMFC = false // use MLIR-based firrtl compiler
+  val useMFC = true // use MLIR-based firrtl compiler
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   if (useMFC) {
     (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
