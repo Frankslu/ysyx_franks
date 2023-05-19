3f14d7b0e2e16973b16f07adf01ba3363718bc58
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-19 12:28:33 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:28:33 up 56 min,  1 user,  load average: 0.56, 0.70, 0.63

--- npc/src/src/memory/fetch.scala
+++ npc/src/src/memory/fetch.scala
@@ -13,38 +13,38 @@ import cpucore.pipeline._
 //   val rdata = Output(UInt(32.W))
 // }
 
-class mem2 extends Module {
-    val io = IO(Flipped(new sram_io(1,1)))
-	// val io = IO(new io1)
-	val Memory = Module(new i_mem())
-	Memory.io <> io
+// class mem2 extends Module {
+//     val io = IO(Flipped(new sram_io(1,1)))
+// 	// val io = IO(new io1)
+// 	val Memory = Module(new i_mem())
+// 	Memory.io <> io
 
-}
+// }
 
-class i_mem extends BlackBox with HasBlackBoxInline {
-	val io = IO(Flipped(new sram_io(1,1)))
-	// val io = IO(new io1)
-	setInline("Memory.v",
-	"""/* verilator lint_off UNUSEDSIGNAL */
-   | module Memory(
-   | input  en,
-   | input  wr,
-   | input  [31:0]  addr,
-   | input  [31:0]  wdata,
-   | input  [3:0]   wstrb,
-   | output [31:0] rdata
-   | );
-   | /* verilator lint_off WIDTHEXPAND */
-   | /* verilator lint_off LATCH */
-   | import "DPI-C" function void pmem_fetch(
-   | input int raddr, output int rdata);
-   | always @(*) begin
-   |   if(en) begin
-   |       pmem_fetch(addr, rdata);
-   |   end
-   |end
-   |
-   |endmodule
-   |
-	""".stripMargin)
-}
+// class i_mem extends BlackBox with HasBlackBoxInline {
+// 	val io = IO(Flipped(new sram_io(1,1)))
+// 	// val io = IO(new io1)
+// 	setInline("Memory.v",
+// 	"""/* verilator lint_off UNUSEDSIGNAL */
+//    | module Memory(
+//    | input  en,
+//    | input  wr,
+//    | input  [31:0]  addr,
+//    | input  [31:0]  wdata,
+//    | input  [3:0]   wstrb,
+//    | output [31:0] rdata
+//    | );
+//    | /* verilator lint_off WIDTHEXPAND */
+//    | /* verilator lint_off LATCH */
+//    | import "DPI-C" function void pmem_fetch(
+//    | input int raddr, output int rdata);
+//    | always @(*) begin
+//    |   if(en) begin
+//    |       pmem_fetch(addr, rdata);
+//    |   end
+//    |end
+//    |
+//    |endmodule
+//    |
+// 	""".stripMargin)
+// }
