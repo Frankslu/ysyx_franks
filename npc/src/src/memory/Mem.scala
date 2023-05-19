1cbb98e6923521b673a3c0562dde976ab4c5a782
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-19 12:44:09 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:44:09 up  1:12,  1 user,  load average: 0.92, 0.77, 0.72

--- npc/src/src/memory/Mem.scala
+++ npc/src/src/memory/Mem.scala
@@ -18,10 +18,8 @@ class mem1 extends Module {
     val io2 = IO(Flipped(new sram_io(0,1)))
 	// val io = IO(new io1)
 	val Memory = Module(new d_mem())
-	// (Memory.io: Data).waiveAll :<>= (io1: Data).waiveAll
-    // (Memory.io: Data).waiveAll :<>= (io2: Data).waiveAll
-    Memory.io :<>= io1
-    Memory.io :<>= io2
+	(Memory.io: Data).waiveAll :<>= (io1: Data).waiveAll
+    (Memory.io: Data).waiveAll :<>= (io2: Data).waiveAll
 }
 
 class d_mem extends BlackBox with HasBlackBoxInline {
