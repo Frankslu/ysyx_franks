606dcb392b4ead25e0beef8be898af387c7eaec2
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-21 00:11:26 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:11:26 up 50 min,  1 user,  load average: 0.41, 0.36, 0.37

--- npc/src/src/main.scala
+++ npc/src/src/main.scala
@@ -1,7 +1,14 @@
-// import chisel3._
-// import chisel3.util._
-// import cpucore._
+import chisel3._
+import chisel3.util._
 
-// class Main extends Module{
-// 	v
-// }
+import memory._
+import cpucore._
+
+class Main extends Module{
+	val cpucore = Module(new mycpu_top)
+	val dram	= Module(new d_mem)
+	val iram	= Module(new i_mem)
+
+	cpucore.data_sram <> dram.io
+	cpucore.inst_sram <> iram.io
+}
