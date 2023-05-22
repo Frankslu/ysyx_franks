333637c073aedc646c45116e41d17a94cf7f4ddd
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-23 03:08:11 +0800

    > sim RTL 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:08:11 up  4:24,  1 user,  load average: 0.47, 0.40, 0.44

--- npc/csrc/src/memory/vaddr.cpp
+++ npc/csrc/src/memory/vaddr.cpp
@@ -3,33 +3,39 @@
 #include "paddr.h"
 #include "ve.h"
 
+extern VMain *top;
+
 void record_read(vaddr_t addr);
 void record_write(vaddr_t addr);
 
 extern "C" void vaddr_fetch(int raddr, int *rdata){
-	*rdata = paddr_read(raddr, 4);
+	if(top->reset == 0)
+		*rdata = paddr_read(raddr, 4);
 }
 
 extern "C" void vaddr_read(int raddr, int *rdata){
-	IFDEF(CONFIG_MTRACE, record_read(raddr));
-	*rdata = paddr_read(raddr, 4);
+	if(top->reset == 0){
+		IFDEF(CONFIG_MTRACE, record_read(raddr));
+		*rdata = paddr_read(raddr, 4);}
 }
 extern "C" void vaddr_write(int waddr, int wdata, char wmask, int *rdata){
-	IFDEF(CONFIG_MTRACE, record_write(waddr));
-	*rdata = 0xbeef;
-	uint32_t mask = (uint32_t)wmask;
-	int len;
-	if (mask == 1){
-		len = 1;
+	if(top->reset == 0){
+		IFDEF(CONFIG_MTRACE, record_write(waddr));
+		*rdata = 0xbeef;
+		uint32_t mask = (uint32_t)wmask;
+		int len;
+		if (mask == 1){
+			len = 1;
+		}
+		else if (mask == 3){
+			len = 2;
+		}
+		else if (mask == 7){
+			len = 4;
+		}
+		
+		paddr_write(waddr, len, wdata);
 	}
-	else if (mask == 3){
-		len = 2;
-	}
-	else if (mask == 7){
-		len = 4;
-	}
-	
-	paddr_write(waddr, len, wdata);
 }
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
