dd61245ef6885467abeaad800c96c1453182a96d
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-24 04:57:02 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:57:02 up 17 min,  1 user,  load average: 0.44, 0.53, 0.37

--- npc/csrc/src/exec/difftest.c
+++ npc/csrc/src/exec/difftest.c
@@ -51,9 +51,7 @@ void difftest_skip_dut(int nr_ref, int nr_dut) {
 	}
 }
 
-void init_difftest(const char *_so_file, int img_size){
-	char ref_so_file[] = "/home/franks/ysyx-workbench/nemu/build/loongarch32r-nemu-interpreter-so";
-	printf("%s\n", ref_so_file);
+void init_difftest(const char *ref_so_file, int img_size){
 	assert(ref_so_file != NULL);
 
 	void *handle;
