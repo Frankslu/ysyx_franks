82c497be8063a576acc3df712f7c1605ccf00f8f
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-24 17:50:00 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:50:00 up  3:01,  1 user,  load average: 0.13, 0.31, 0.39

--- abstract-machine/am/src/loongarch/npc/cte.c
+++ abstract-machine/am/src/loongarch/npc/cte.c
@@ -1,4 +1,5 @@
 #include <am.h>
+#include <loongarch/loongarch32r.h>
 #include <klib.h>
 
 static Context* (*user_handler)(Event, Context*) = NULL;
@@ -6,7 +7,8 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    switch (c->mcause) {
+    uintptr_t ecode = 0;
+    switch (ecode) {
       default: ev.event = EVENT_ERROR; break;
     }
 
@@ -21,7 +23,7 @@ extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
-  asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
+  asm volatile("csrwr %0, 0xc" : : "r"(__am_asm_trap));  // 0xc = eentry
 
   // register event handler
   user_handler = handler;
@@ -34,7 +36,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  asm volatile("li a7, -1; ecall");
+  asm volatile("li.w $a7, -1; syscall 0");
 }
 
 bool ienabled() {
