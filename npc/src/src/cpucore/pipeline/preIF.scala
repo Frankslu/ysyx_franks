cd10ecc4f2ebe87013f548e78757012e1e30d1ff
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-24 13:58:35 +0800

    > generate verilog 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:58:35 up 40 min,  1 user,  load average: 0.57, 0.22, 0.20

--- npc/src/src/cpucore/pipeline/preIF.scala
+++ npc/src/src/cpucore/pipeline/preIF.scala
@@ -12,9 +12,9 @@ class preIF extends Module{
     val inst_sram = IO(new sram_io)
     val tofs = IO(Decoupled(new pre2fs))
 
-    val pc = Reg(UInt(ADDR_WIDTH.W))
+    val pc = RegInit(PC_INIT.U(ADDR_WIDTH.W))
     val snpc = pc + 4.U
-    pc := Mux(reset.asBool, PC_INIT.U, Mux(br.taken, br.target, snpc))
+    pc := Mux(br.taken, br.target, snpc)
 
     inst_sram.en := ~reset.asBool
     inst_sram.wr := 0.B
