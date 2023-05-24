d3788a5dfe7c462e5ecdc11a3b04b0d5b64bdf34
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-24 23:19:37 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:19:37 up  8:30,  1 user,  load average: 0.65, 0.54, 0.28

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,7 +40,7 @@ __attribute__((unused)) static void trace_and_difftest(Decode *_this, vaddr_t dn
 #ifdef CONFIG_ITRACE_COND
 	if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
-	if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+	// if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
 	IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 	
 	IFDEF(CONFIG_IRING, iring_write(_this->logbuf));
