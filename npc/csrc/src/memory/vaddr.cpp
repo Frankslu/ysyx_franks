74538557ef3fcb4d4df7b6207a164a5a4dd4452b
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-25 01:52:14 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:52:14 up 11:03,  1 user,  load average: 0.32, 0.30, 0.30

--- npc/csrc/src/memory/vaddr.cpp
+++ npc/csrc/src/memory/vaddr.cpp
@@ -25,6 +25,7 @@ extern "C" void vaddr_write(int waddr, int wdata, char wmask, int *rdata){
 	IFDEF(CONFIG_MTRACE, record_write(waddr));
 	*rdata = 0xbeef;
 	uint32_t mask = (uint32_t)wmask;
+	printf("wmask = %u\n", mask);
 	int len;
 	if (mask == 1){
 		len = 1;
