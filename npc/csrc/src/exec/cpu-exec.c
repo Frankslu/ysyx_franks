92b16b2fdaf126aa6d8f01a26fa94b85266a74e5
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-24 12:09:39 +0800

    > compile NEMU 22051115 陈卓 Linux franks-ThinkPad-X1-Carbon-7th 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:09:39 up  1:26,  1 user,  load average: 0.20, 0.35, 0.29

--- npc/csrc/src/exec/cpu-exec.c
+++ npc/csrc/src/exec/cpu-exec.c
@@ -63,7 +63,7 @@ static void exec_once(Decode *s) {
 
 #ifdef CONFIG_ITRACE
 	char *p = s->logbuf;
-	p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", cpu.pc);
+	p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
 	int ilen = s->snpc - s->pc;
 	int i;
 	uint8_t *inst = (uint8_t *)&s->isa.inst.val;
